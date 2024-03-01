f751dffa87d8f9acd2a44e33251fa06189762e92
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-03-01 04:27:30 +0200

    fix BASE_URL

--- src/helpers/constants.jsx
+++ src/helpers/constants.jsx
@@ -1,6 +1,6 @@
-// export const BASE_URL = 'https://bloomskill-backend.vercel.app/api';
+export const BASE_URL = 'https://bloomskill-backend.vercel.app/api';
 // export const BASE_URL_IMG = 'https://event-shop-backend.vercel.app/uploads/';
-export const BASE_URL = 'http://localhost:3030/api';
+// export const BASE_URL = 'http://localhost:3030/api';
 
 export const BASE_URL_IMG = 'https://res.cloudinary.com/dskedijqb/image/upload/v1706382942/bloomskill_event/';
 export const BASE_URL_AVATAR = 'https://res.cloudinary.com/dskedijqb/image/upload/v1706382942/bloomskill_specialist/';
